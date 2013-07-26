ce9a40c6bfa5d11f0f5b8e271f9568e1762a7ddf
Aktau <nicolashillegeer@gmail.com>
2013-07-26 22:06:02 +0200

    future-proof, convert to github.io instead of github.com

--- templates.lua
+++ templates.lua
@@ -19,7 +19,7 @@ end
 
 function github_pages(domain, username)
     -- Create CNAME for www.yourdomain.com pointing to user.github.com
-    cname(concat("www", domain), concat(username, "github.com"))
+    cname(concat("www", domain), concat(username, "github.io"))
 
     -- Create A records for root/naked domain (yourdomain.com)
     a(domain, "204.232.175.78")
